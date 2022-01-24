1edee02ff626b2cec8263e15c6592cbcbd8d2596
zoltanszanyi <81174081+zoltanszanyi@users.noreply.github.com>
2022-01-24 19:21:44 +0100

    Delete teammate_finder_app directory

--- teammate_finder_app/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class TeammateFinderAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'teammate_finder_app'
-
-    def ready(self):
-        import teammate_finder_app.signals
--- teammate_finder_app/filters.py
+++
@@ -1,7 +0,0 @@
-import django_filters
-from .models import *
-
-class Filter(django_filters.FilterSet):
-    class Meta:
-        model = Profile
-        fields = ('gender', 'language', 'region', 'country', 'rank', 'role1','role2','role3','role4','role5') 
